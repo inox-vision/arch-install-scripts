90d3a04bac21d1c3316a74b3ca77ee9c239c93db
Adrian Niwa <mail@adrianniwa.com>
2025-02-11 18:30:30 +0100

    stripping user part

--- 2-inox-arch-chroot-part.sh
+++ 2-inox-arch-chroot-part.sh
@@ -18,6 +18,15 @@ echo "127.0.0.1		localhost
 ::1		localhost
 127.0.1.1	$hostname.localdomain		$hostname" >> /etc/hosts
 
+################
+# BASIC INSTAL #
+################
+
+pacman -S reflector
+reflector -c Germany -a 6 --download-timeout 20 --connection-timeout 20 -l 7 --sort rate --save /etc/pacman.d/mirrorlist
+sed -i 's/#Color/Color/g' /etc/pacman.conf
+sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5/g' /etc/pacman.conf
+
 INSTALL="
 	grub
 	neovim
@@ -27,125 +36,14 @@ INSTALL="
 	linux-headers
 	sudo
 	"
-Window_manager="
-	xorg
-	mesa
-	qtile 
-	lxappearance
-	nitrogen
-	picom
-	lightdm
-	lightdm-gtk-greeter
-	"
-
-BASIC_utils="
-	inxi
-	htop
-	duf
-	speedtest-cli
-	xarchiver
-	p7zip
-	unzip
-	"
-
-SYSTEM_utils="
-	xfce4-terminal
-	xfce4-power-manager
-	bash-completion
-	python-psutil
-	powertop
-	tuned
-	auto-cpufreq
-	sxhkd
-	xclip
-	apparmor
-	firewalld
-	cifs-utils
-	"	
-
-THEMING="
-	gtk3
-	qt5
-	breeze-gtk
-	ttf-roboto
-	ttf-opensans
-	ttf-ms-fonts
-	ttf-font-awesome
-	ttf-mononoki-nerd
-	ttf-hack-nerd
-	adobe-source-code-pro-fonts
-	adwaita-cursors
-	adwaita-icon-theme
-	"
-
-OTHER_apps="
-	thunderbird
-	brave-bin
-	qbittorrent
-	flameshot
-	evince
-	libreoffice-still
-	ristretto
-	thunar
-	ffmpeg
-	ffmpegthumbnailer
-	tumbler
-	code
-	whatsdesk-bin
-	mpv
-	nextcloud-client
-	"
-
-VIRTUAL_machines="
-	virt-manager
-	qemu
-	ovmf
-	vde2
-	dnsmasq
-	bridge-utils
-	docker
-	"
-
-SOUND="
-	alsa-utils
-	pipewire
-	pipewire-alsa
-	pipewire-jack
-	pipewire-pulse
-	"
-
-PRINTING="
-	brother-mfc-j5910dw
-	cups
-	cups-filters
-	system-config-printer
-	"
-
-echo -e "Installing YAY \n"
-sleep 2
-
-mkdir /home/adrian/git_clones
-cd /home/adrian/git_clones
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-
-pacman -S reflector
-reflector -c Germany -a 6 --download-timeout 20 --connection-timeout 20 -l 7 --sort rate --save /etc/pacman.d/mirrorlist
-sed -i 's/#Color/Color/g' /etc/pacman.conf
-sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5/g' /etc/pacman.conf
-
 
 pacman -S $INSTALL
-pacman -S $Window_manager
-yay -S $BASIC_utils
-yay -S $SYSTEM_utils
-yay -S $THEMING
-yay -S $OTHER_apps
-yay -S $VIRTUAL_machines
-yay -S $SOUND
-yay -S $PRINTING
 
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+
+systemctl enable lightdm.service
+systemctl enable NetworkManager
 
 
 ################
@@ -163,113 +61,14 @@ passwd $username
 sed -i 's/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
 sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 
-usermod -a -G libvirt $username
 mkdir /home/$username/CLOUD
 mkdir /home/$username/virt_machines
 
-
-######################
-#  EABLING SERVICES  #
-######################
+git clone https://github.com/inox-vision/arch-install-scripts.git /home/$username/
 
 
-systemctl enable lightdm.service
-systemctl enable NetworkManager
-systemctl enable apparmor.service
-systemctl enable firewalld.service
-systemctl enable cups
-systemctl enable --global pipewire.service
-systemctl enable libvirtd.service
+su - $username
 
 
-
-###################
-#  CONFIGURATION  #
-###################
-
-echo "setxkbmap pl
-source ~/.bashrc" > /home/$username/.bash_profile
-
-## NEOVIM
-
-echo ":set tabstop=4
-:set shiftwidth=4
-:set expandtab
-:set clipboard+=unnamedplus" > /home/$username/.vimrc
-
-
-## SOUND BUTTONS
-
-echo "XF86AudioRaiseVolume
-    amixer -c 0 set Master 1dB+
-
-XF86AudioLowerVolume
-    amixer -c 0 set Master 1dB-
-
-XF86AudioMute
-    amixer -c 0 set Master 1+ toggle" > /home/$username/.sxhkdrc
-
-## XPROFILE
-
-echo "
-sxhkd -c ~/.sxhkdrc &
-nitrogen --restore
-picom &
-xfce4-power-manager &
-flameshot &
-nextcloud &
-setxkbmap pl" > /home/$username/.xprofile
- 
-## WALLPAPER
-
-mkdir /home/$username/Pictures
-cp arch-install-scripts/wallpaperflare.com_wallpaper2.jpg /home/$username/Pictures/wallpaperflare.com_wallpaper2.jpg
-echo "[xin_-1]
-file=/home/$username/Pictures/wallpaperflare.com_wallpaper2.jpg
-mode=4
-bgcolor=#000000" > /home/$username/.config/nitrogen/bg-saved.cfg
-
-
-echo "<network>
-  <name>br10</name>
-  <forward mode='nat'>
-    <nat>
-      <port start='1024' end='65535'/>
-    </nat>
-  </forward>
-  <bridge name='br10' stp='on' delay='0'/>
-  <ip address='192.168.30.1' netmask='255.255.255.0'>
-    <dhcp>
-      <range start='192.168.30.50' end='192.168.30.200'/>
-    </dhcp>
-  </ip>
-</network>" > /home/$username/virt_machines/br.xml
-
-
-## QTILE
-
-cp arch-install-scripts/config.py /home/$username/.config/qtile/
-cp arch-install-scripts/xfce4-terminal.xml /home/$username/.config/xfce4/xfconf/xfce-perchannel-xml/
-cp arch-install-scripts/xfce4-power-manager.xml /home/$username/.config/xfce4/xfconf/xfce-perchannel-xml/
-cp arch-install-scripts/thunar.xml /home/$username/.config/xfce4/xfconf/xfce-perchannel-xml/
-cp arch-install-scripts/init.vim /home/$username/.config/nvim/
-cp arch-install-scripts/.bashrc /home/$username/
-
-virsh net-create /home/$username/virt_machines/br.xml
-virsh net-autostart br10
-
-chown -R $username:$username /home/$username
-
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
-
-
-
-# MKINITCPIO - encrypt dodać w hooks
-
-# GRUB cryptdevice=UUID=device-UUID:root root=/dev/mapper/root w kernel parameters
-
-# Skopiować plik konfiguracyjny myszki z /etc/X11/xorg.conf
-# Utworzyć systemd unit dla powertop i uruchomić
 
 
---
+++ 3-user-part.sh
@@ -0,0 +1,213 @@
+#! /bin/bash
+
+Window_manager="
+	xorg
+	mesa
+	qtile 
+	lxappearance
+	nitrogen
+	picom
+	lightdm
+	lightdm-gtk-greeter
+	"
+
+BASIC_utils="
+	inxi
+	htop
+	duf
+	speedtest-cli
+	xarchiver
+	p7zip
+	unzip
+	"
+
+SYSTEM_utils="
+	xfce4-terminal
+	xfce4-power-manager
+	bash-completion
+	python-psutil
+	powertop
+	tuned
+	auto-cpufreq
+	sxhkd
+	xclip
+	apparmor
+	firewalld
+	cifs-utils
+	"	
+
+THEMING="
+	gtk3
+	qt5
+	breeze-gtk
+	ttf-roboto
+	ttf-opensans
+	ttf-ms-fonts
+	ttf-font-awesome
+	ttf-mononoki-nerd
+	ttf-hack-nerd
+	adobe-source-code-pro-fonts
+	adwaita-cursors
+	adwaita-icon-theme
+	"
+
+OTHER_apps="
+	thunderbird
+	brave-bin
+	qbittorrent
+	flameshot
+	evince
+	libreoffice-still
+	ristretto
+	thunar
+	ffmpeg
+	ffmpegthumbnailer
+	tumbler
+	code
+	whatsdesk-bin
+	mpv
+	nextcloud-client
+	"
+
+VIRTUAL_machines="
+	virt-manager
+	qemu
+	ovmf
+	vde2
+	dnsmasq
+	bridge-utils
+	docker
+	"
+
+SOUND="
+	alsa-utils
+	pipewire
+	pipewire-alsa
+	pipewire-jack
+	pipewire-pulse
+	"
+
+PRINTING="
+	brother-mfc-j5910dw
+	cups
+	cups-filters
+	system-config-printer
+	"
+
+echo -e "Installing YAY \n"
+sleep 3
+
+mkdir $HOME/git_clones
+cd $HOME/git_clones
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+
+yay -S $Window_manager
+yay -S $BASIC_utils
+yay -S $SYSTEM_utils
+yay -S $THEMING
+yay -S $OTHER_apps
+yay -S $VIRTUAL_machines
+yay -S $SOUND
+yay -S $PRINTING
+
+
+######################
+#  EABLING SERVICES  #
+######################
+
+
+
+sudo systemctl enable apparmor.service
+sudo systemctl enable firewalld.service
+sudo systemctl enable cups
+sudo systemctl enable --global pipewire.service
+sudo systemctl enable libvirtd.service
+sudo usermod -a -G libvirt $USER
+
+
+
+###################
+#  CONFIGURATION  #
+###################
+
+echo "setxkbmap pl
+source ~/.bashrc" > $HOME/.bash_profile
+
+## NEOVIM
+
+echo ":set tabstop=4
+:set shiftwidth=4
+:set expandtab
+:set clipboard+=unnamedplus" > $HOME/.vimrc
+
+
+## SOUND BUTTONS
+
+echo "XF86AudioRaiseVolume
+    amixer -c 0 set Master 1dB+
+
+XF86AudioLowerVolume
+    amixer -c 0 set Master 1dB-
+
+XF86AudioMute
+    amixer -c 0 set Master 1+ toggle" > $HOME/.sxhkdrc
+
+## XPROFILE
+
+echo "
+sxhkd -c ~/.sxhkdrc &
+nitrogen --restore
+picom &
+xfce4-power-manager &
+flameshot &
+nextcloud &
+setxkbmap pl" > $HOME/.xprofile
+ 
+## WALLPAPER
+
+mkdir $HOME/Pictures
+cp arch-install-scripts/wallpaperflare.com_wallpaper2.jpg $HOME/Pictures/wallpaperflare.com_wallpaper2.jpg
+echo "[xin_-1]
+file=$HOME/Pictures/wallpaperflare.com_wallpaper2.jpg
+mode=4
+bgcolor=#000000" > $HOME/.config/nitrogen/bg-saved.cfg
+
+
+echo "<network>
+  <name>br10</name>
+  <forward mode='nat'>
+    <nat>
+      <port start='1024' end='65535'/>
+    </nat>
+  </forward>
+  <bridge name='br10' stp='on' delay='0'/>
+  <ip address='192.168.30.1' netmask='255.255.255.0'>
+    <dhcp>
+      <range start='192.168.30.50' end='192.168.30.200'/>
+    </dhcp>
+  </ip>
+</network>" > $HOME/virt_machines/br.xml
+
+
+## QTILE
+
+cp arch-install-scripts/config.py $HOME/.config/qtile/
+cp arch-install-scripts/xfce4-terminal.xml $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/
+cp arch-install-scripts/xfce4-power-manager.xml $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/
+cp arch-install-scripts/thunar.xml $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/
+cp arch-install-scripts/init.vim $HOME/.config/nvim/
+cp arch-install-scripts/.bashrc $HOME/
+sudo cp arch-install-scripts/30-touchpad.conf /etc/X11/xorg.conf.d/
+
+sudo virsh net-create $HOME/virt_machines/br.xml
+sudo virsh net-autostart br10
+
+
+
+# MKINITCPIO - encrypt dodać w hooks
+
+# GRUB cryptdevice=UUID=device-UUID:root root=/dev/mapper/root w kernel parameters
+
